fd4e55637797a658b77848f8031aa296e18ae333
Ammar2214 <ahmadammar0412@gmail.com>
2025-04-06 12:26:27 +1000

    title change

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Header"
 import Owner from "./owner"
 import Announcement from "./announcement"
 import ReportForm from "./Form"
+import Head from "./Head"
 
 function App() {
   
   return (
     <>
+    <Head/>
     <Header/>
     <Announcement/>
     <Owner/>
---
+++ src/Head.jsx
@@ -0,0 +1,15 @@
+
+function Head(){
+    return(
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>D'Langat Residence</title>
+     </head>
+
+    );
+
+}
+
+
+export default Head
